3358deda2621f2bf479142e8e1abf3cf096a5d3e
ThemisZ <tzi.ch@ANYACCESS.NET>
2016-08-24 15:52:23 +0200

    Assignment 2 - Themis Zinoulis
    2nd attempt to commit file...

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ######################################################
 ##                                                  ##
-##  Assignment 2: Caching the Inverse of a Matrix   ##
+##  Assignment 2: Caching the Inverse of a Matrix 	##
 ##                                                  ##
 ######################################################
 
